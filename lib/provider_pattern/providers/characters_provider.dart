aea0abce4c49dd970b3ea7bb8330b9c8578560e7
Shahanur Zaman <shatil200@gmail.com>
2024-07-11 17:14:29 +0600

    -Add gender selection ui -Refactor code

---
+++ lib/provider_pattern/providers/characters_provider.dart
@@ -0,0 +1,8 @@
+
+import 'package:flutter/material.dart';
+
+class CharactersProvider extends ChangeNotifier{
+  String gender="male";
+  String? imagePath;
+  String promptText="";
+}
---
+++ lib/provider_pattern/screens/gender_selection_widget.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:talkie_ai/provider_pattern/providers/characters_provider.dart';
+
+class GenderSelectionWidget extends StatefulWidget {
+  const GenderSelectionWidget({super.key});
+
+  @override
+  _GenderSelectionWidgetState createState() => _GenderSelectionWidgetState();
+}
+
+class _GenderSelectionWidgetState extends State<GenderSelectionWidget> {
+  String _selectedGender = '';
+
+  void _onGenderSelected(String gender) {
+    setState(() {
+      _selectedGender = gender;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                _buildGenderOption('assets/male.png', 'Boy', 'male'),
+                _buildGenderOption('assets/female.png', 'Girl', 'female'),
+                _buildGenderOption(
+                    'assets/non_binary.png', 'Non-Binary', 'non_binary'),
+              ],
+            ),
+            const SizedBox(height: 40),
+            SizedBox(
+              width: MediaQuery.of(context).size.width-60,
+              height: 40,
+              child: ElevatedButton(
+                onPressed: _selectedGender.isNotEmpty ? _startAction : null,
+                style: ElevatedButton.styleFrom(
+                  foregroundColor:
+                      _selectedGender.isNotEmpty ? Colors.yellow : Colors.grey,
+                  textStyle: const TextStyle(fontSize: 18),
+                ),
+                child: const Text('Start'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildGenderOption(String assetPath, String label, String gender) {
+    bool isSelected = _selectedGender == gender;
+    return GestureDetector(
+      onTap: () => _onGenderSelected(gender),
+      child: Container(
+        padding: const EdgeInsets.all(8.0),
+        decoration: BoxDecoration(
+          border: Border.all(color: isSelected ? Colors.yellow : Colors.grey),
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+        child: Column(
+          children: [
+            Image.asset(
+              assetPath,
+              width: MediaQuery.of(context).size.width / 5,
+              height: MediaQuery.of(context).size.width / 5,
+            ),
+            const SizedBox(height: 8),
+            Text(
+              label,
+              style: const TextStyle(color: Colors.white),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _startAction() {
+    Provider.of<CharactersProvider>(context, listen: false).gender =
+        _selectedGender;
+    //Navigator.of(context).push();
+  }
+}
